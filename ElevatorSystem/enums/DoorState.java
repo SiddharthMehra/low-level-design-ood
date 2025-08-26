23270fc7cec258dcc20cb6645578801a1a3e9f65
Siddharth Mehra <siddharth.mehra24@gmail.com>
2025-08-26 12:16:29 +0530

    added enum for elevator state

---
+++ ElevatorSystem/enums/DoorState.java
@@ -0,0 +1,5 @@
+package ElevatorSystem.enums;
+
+public enum DoorState {
+    OPEN, CLOSED
+}
---
+++ ElevatorSystem/enums/ElevatorState.java
@@ -0,0 +1,6 @@
+package ElevatorSystem.enums;
+
+public enum ElevatorState {
+
+    IDLE, UP, DOWN, MAINTENANCE
+}
